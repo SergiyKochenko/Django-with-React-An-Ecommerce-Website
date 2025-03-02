d6565908ce629ad1898cc61d8712a644a6dbf65e
Sergiy <sergiykochenko@gmail.com>
2025-03-02 19:31:32 +0000

    Added Cart Reducer and Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,21 @@ import { configureStore } from '@reduxjs/toolkit'
 import { thunk } from 'redux-thunk'
 import { combineReducers } from 'redux'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
-  productDetails: productDetailsReducer
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage }
+}
 
 const middleware = [thunk]
 
