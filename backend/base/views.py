ef479d51348a5a83f78c3b1eb2e5c10b322253bb
Sergiy <sergiykochenko@gmail.com>
2025-03-03 23:51:08 +0000

    JWT Costomization made

--- backend/base/views.py
+++ backend/base/views.py
@@ -7,29 +7,47 @@ from .models import Product
 from .products import products
 from .serializer import ProductSerializer
 
-# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-@api_view(['GET'])
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data["username"] = self.user.username
+        data["email"] = self.user.email
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(["GET"])
 def getRouters(request):
     routes = [
-        'api/products/',
-        'api/products/create/',
-        'api/products/upload/',
-        'api/products/<id>/reviews/',
-        'api/products/top/',
-        'api/products/<id>/',
-        'api/products/delete/<id>/',
-        'api/products/<update>/<id>/',
+        "api/products/",
+        "api/products/create/",
+        "api/products/upload/",
+        "api/products/<id>/reviews/",
+        "api/products/top/",
+        "api/products/<id>/",
+        "api/products/delete/<id>/",
+        "api/products/<update>/<id>/",
     ]
     return Response(routes)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
