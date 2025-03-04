381ae9eaeb5ab92cb4b4678a58086b8b576e5b51
Sergiy <sergiykochenko@gmail.com>
2025-03-04 21:51:52 +0000

    Added Register User

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path(
         "users/login", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"
     ),
-    path("", views.getRouters, name="routes"),
+    path("users/register/", views.registerUser, name="register"),
     path("users/profile/", views.getUserProfile, name="users-profile"),
     path("users/", views.getUsers, name="users"),
     path("products/", views.getProducts, name="products"),
