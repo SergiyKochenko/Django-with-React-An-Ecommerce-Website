025bdd63077ba6cb3252fa4b8eb9a94715dfc55b
Sergiy <sergiykochenko@gmail.com>
2025-03-04 20:17:16 +0000

    Created User Serializer

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
         "users/login", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"
     ),
     path("", views.getRouters, name="routes"),
+    path("users/profile/", views.getUserProfile, name="users-profile"),
     path("products/", views.getProducts, name="products"),
     path("products/<str:pk>/", views.getProduct, name="product"),
 ]
