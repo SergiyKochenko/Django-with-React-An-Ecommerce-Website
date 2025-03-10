1461d325ce028a1fddf2d210e68cda4e5e7509fe
Sergiy <sergiykochenko@gmail.com>
2025-03-10 20:58:34 +0000

    Created User List Reducer, Action & Screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -41,6 +41,22 @@ function Header () {
                   <i className='fas fa-user'></i> Login
                 </Nav.Link>
               )}
+
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
+                  <NavDropdown.Item as={Link} to='/admin/userlist'>
+                    Users
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='/admin/productlist'>
+                    Products
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='/admin/orderlist'>
+                    Orders
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
+              
+
             </Nav>
           </Navbar.Collapse>
         </Container>
