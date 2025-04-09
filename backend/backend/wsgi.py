b70ea547ed09608509773ac9c5ee440ca27fd3c0
Sergiy <sergiykochenko@gmail.com>
2025-04-09 12:26:38 +0100

    Fix os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+# Explicitly set the DJANGO_SETTINGS_MODULE
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 
 application = get_wsgi_application()
