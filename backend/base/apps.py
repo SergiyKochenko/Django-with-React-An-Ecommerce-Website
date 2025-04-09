99f8f18890d6b596bf2089b445b192a5aaaf8c89
Sergiy <sergiykochenko@gmail.com>
2025-04-09 12:50:10 +0100

    Fix backend.base.apps.BaseConfig

--- backend/base/apps.py
+++ backend/base/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
 
-
 class BaseConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "base"
-
-    def ready(self):
-        import base.signals
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'backend.base'  # Ensure this matches the full path of the app
